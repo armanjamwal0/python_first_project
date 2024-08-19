46cadba3459ae7af6d79304371e0fc06c2aad5ea
armanjamwal0 <160482497+armanjamwal0@users.noreply.github.com>
2024-08-19 14:05:19 +0530

    Update First_python_project.py

--- First_python_project.py
+++ First_python_project.py
@@ -1,7 +1,7 @@
 #My first pyhon project
 
 user_name = print(" User Name Genarater ")
-First_Name = input("Enter Your First Name : ")
-Last_Name =  input("Enter Your Last Name : ")
+First_Name = input("Enter Your First Name :\n ")
+Last_Name =  input("Enter Your Last Name :\n ")
 print("Your name are genrating.............")
 print("your fullname is "+First_Name+Last_Name)
