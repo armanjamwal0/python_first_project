0fe176a5812c8471bb37fe3d83fcfafaefb8a4e5
armanjamwal0 <160482497+armanjamwal0@users.noreply.github.com>
2024-08-19 13:57:24 +0530

    Add files via upload
    my first Python project

---
+++ First_python_project.py
@@ -0,0 +1,7 @@
+#My first pyhon project
+
+user_name = print(" User Name Genarater ")
+First_Name = input("Enter Your First Name : ")
+Last_Name =  input("Enter Your Last Name : ")
+print("Your name are genrating.............")
+print("your fullname is "+First_Name+Last_Name)
